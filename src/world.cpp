460079f191784a414adf2e90291d0ca21d264670
Sam Chow <sam@samc.how>
2019-01-25 15:57:16 -0800

    remove turtles and fish, change control sceme

--- src/world.cpp
+++ src/world.cpp
@@ -11,11 +11,6 @@
 // Same as static in c, local to compilation unit
 namespace
 {
-	const size_t MAX_TURTLES = 15;
-	const size_t MAX_FISH = 5;
-	const size_t TURTLE_DELAY_MS = 2000;
-	const size_t FISH_DELAY_MS = 5000;
-
 	namespace
 	{
 		void glfw_err_cb(int error, const char* desc)
@@ -25,10 +20,7 @@ namespace
 	}
 }
 
-World::World() : 
-	m_points(0),
-	m_next_turtle_spawn(0.f),
-	m_next_fish_spawn(0.f)
+World::World()
 {
 	// Seeding rng with random device
 	m_rng = std::default_random_engine(std::random_device()());
@@ -119,8 +111,6 @@ bool World::init(vec2 screen)
 	
 	fprintf(stderr, "Loaded music\n");
 
-	m_current_speed = 1.f;
-
 	return m_salmon.init() && m_water.init();
 }
 
@@ -139,15 +129,7 @@ void World::destroy()
 	Mix_CloseAudio();
 
 	m_salmon.destroy();
-	for (auto& turtle : m_turtles)
-		turtle.destroy();
-	for (auto& fish : m_fish)
-		fish.destroy();
-	for (auto& missile : m_missiles)
-		missile.destroy();
-	m_turtles.clear();
-	m_fish.clear();
-	m_missiles.clear();
+
 	glfwDestroyWindow(m_window);
 }
 
@@ -158,146 +140,12 @@ bool World::update(float elapsed_ms)
         glfwGetFramebufferSize(m_window, &w, &h);
 	vec2 screen = { (float)w, (float)h };
 
-	// Checking Salmon - Turtle collisions
-	for (const auto& turtle : m_turtles)
-	{
-		if (m_salmon.collides_with(turtle))
-		{
-			if (m_salmon.is_alive()) {
-				Mix_PlayChannel(-1, m_salmon_dead_sound, 0);
-				m_water.set_salmon_dead();
-			}
-			m_salmon.kill();
-			break;
-		}
-	}
-
-	// Checking Salmon - Fish collisions
-	auto fish_it = m_fish.begin();
-	while (fish_it != m_fish.end())
-	{
-		if (m_salmon.is_alive() && m_salmon.collides_with(*fish_it))
-		{
-			fish_it = m_fish.erase(fish_it);
-			m_salmon.light_up();
-			Mix_PlayChannel(-1, m_salmon_eat_sound, 0);
-			++m_points;
-		}
-		else
-			++fish_it;
-	}
-
-	// check missile - turtle collisions
-	auto missile_it = m_missiles.begin();
-	while (missile_it != m_missiles.end())
-	{
-		// TODO: this is O(n^2), there's gotta be a better way
-		if (m_missiles.empty()) {
-			break;
-		}
-		auto turtle_it = m_turtles.begin();
-
-		while (turtle_it != m_turtles.end()) {
-			if (m_turtles.empty()) {
-				break;
-			}
-			if ((*missile_it).collides_with(*turtle_it))
-			{
-				std::cout << "missile collided with turtle" << std::endl;
-				missile_it = m_missiles.erase(missile_it);
-				turtle_it = m_turtles.erase(turtle_it);
-				break;
-			} else
-			{
-				turtle_it++;
-			}
-		}
-		missile_it++;
-	}
 
 
 	// Updating all entities, making the turtle and fish
 	// faster based on current
 	m_salmon.update(elapsed_ms);
-	for (auto& turtle : m_turtles)
-		turtle.update(elapsed_ms * m_current_speed);
-	for (auto& fish : m_fish)
-		fish.update(elapsed_ms * m_current_speed);
 
-	for (auto& missile : m_missiles)
-		missile.update(elapsed_ms * m_current_speed);
-
-	// Removing out of screen turtles
-	auto turtle_it = m_turtles.begin();
-	while (turtle_it != m_turtles.end())
-	{
-		float w = turtle_it->get_bounding_box().x / 2;
-		if (turtle_it->get_position().x + w < 0.f)
-		{
-			turtle_it = m_turtles.erase(turtle_it);
-			continue;
-		}
-
-		++turtle_it;
-	}
-
-	// Removing out of screen fish
-	fish_it = m_fish.begin();
-	while (fish_it != m_fish.end())
-	{
-		float w = fish_it->get_bounding_box().x / 2;
-		if (fish_it->get_position().x + w < 0.f)
-		{
-			fish_it = m_fish.erase(fish_it);
-			continue;
-		}
-
-		++fish_it;
-	}
-
-	// Removing out of screen missiles (code re-used from the fish check code)
-	missile_it = m_missiles.begin();
-	while (missile_it != m_missiles.end())
-	{
-		float w = missile_it->get_bounding_box().x / 2;
-		if (missile_it->get_position().x + w < 0.f)
-		{
-			std::cout << "removed missile" << std::endl;
-			missile_it = m_missiles.erase(missile_it);
-			continue;
-		}
-
-		++missile_it;
-	}
-
-	// Spawning new turtles
-	m_next_turtle_spawn -= elapsed_ms * m_current_speed;
-	if (m_turtles.size() <= MAX_TURTLES && m_next_turtle_spawn < 0.f)
-	{
-		if (!spawn_turtle())
-			return false;
-
-		Turtle& new_turtle = m_turtles.back();
-	
-		// Setting random initial position
-		new_turtle.set_position({ screen.x + 150, 50 + m_dist(m_rng) * (screen.y - 100) });
-
-		// Next spawn
-		m_next_turtle_spawn = (TURTLE_DELAY_MS / 2) + m_dist(m_rng) * (TURTLE_DELAY_MS/2);
-	}
-
-	// Spawning new fish
-	m_next_fish_spawn -= elapsed_ms * m_current_speed;
-	if (m_fish.size() <= MAX_FISH && m_next_fish_spawn < 0.f)
-	{
-		if (!spawn_fish())
-			return false;
-		Fish& new_fish = m_fish.back();
-
-		new_fish.set_position({ screen.x + 150, 50 + m_dist(m_rng) *  (screen.y - 100) });
-
-		m_next_fish_spawn = (FISH_DELAY_MS / 2) + m_dist(m_rng) * (FISH_DELAY_MS / 2);
-	}
 
 	// If salmon is dead, restart the game after the fading animation
 	if (!m_salmon.is_alive() &&
@@ -306,10 +154,7 @@ bool World::update(float elapsed_ms)
 		glfwGetWindowSize(m_window, &w, &h);
 		m_salmon.destroy();
 		m_salmon.init();
-		m_turtles.clear();
-		m_fish.clear();
 		m_water.reset_salmon_dead_time();
-		m_current_speed = 1.f;
 	}
 
 	return true;
@@ -328,8 +173,6 @@ void World::draw()
 
 	// Updating window title with points
 	std::stringstream title_ss;
-	title_ss << "Points: " << m_points;
-	std::cout << "proints" << m_points;
 	glfwSetWindowTitle(m_window, title_ss.str().c_str());
 
 	/////////////////////////////////////
@@ -358,12 +201,6 @@ void World::draw()
 	mat3 projection_2D{ { sx, 0.f, 0.f },{ 0.f, sy, 0.f },{ tx, ty, 1.f } };
 
 	// Drawing entities
-	for (auto& turtle : m_turtles)
-		turtle.draw(projection_2D);
-	for (auto& fish : m_fish)
-		fish.draw(projection_2D);
-	for (auto& missile : m_missiles)
-		missile.draw(projection_2D);
 	m_salmon.draw(projection_2D);
 
 	/////////////////////
@@ -394,31 +231,6 @@ bool World::is_over()const
 	return glfwWindowShouldClose(m_window);
 }
 
-// Creates a new turtle and if successfull adds it to the list of turtles
-bool World::spawn_turtle()
-{
-	Turtle turtle;
-	if (turtle.init())
-	{
-		m_turtles.emplace_back(turtle);
-		return true;
-	}
-	fprintf(stderr, "Failed to spawn turtle");
-	return false;
-}
-
-// Creates a new fish and if successfull adds it to the list of fish
-bool World::spawn_fish()
-{
-	Fish fish;
-	if (fish.init())
-	{
-		m_fish.emplace_back(fish);
-		return true;
-	}
-	fprintf(stderr, "Failed to spawn fish");
-	return false;
-}
 
 // On key callback
 void World::on_key(GLFWwindow*, int key, int, int action, int mod)
@@ -427,16 +239,16 @@ void World::on_key(GLFWwindow*, int key, int, int action, int mod)
 	{
 		switch (key)
 		{
-		case GLFW_KEY_LEFT:
+		case GLFW_KEY_A:
 			m_salmon.should_move(1, true);
 			break;
-		case GLFW_KEY_RIGHT:
+		case GLFW_KEY_D:
 			m_salmon.should_move(2, true);
 			break;
-		case GLFW_KEY_DOWN:
+		case GLFW_KEY_S:
 			m_salmon.should_move(3, true);
 			break;
-		case GLFW_KEY_UP:
+		case GLFW_KEY_W:
 			m_salmon.should_move(4, true);
 			break;
 		default:
@@ -449,32 +261,17 @@ void World::on_key(GLFWwindow*, int key, int, int action, int mod)
 	{
 		switch (key)
 		{
-		case GLFW_KEY_LEFT:
+		case GLFW_KEY_A:
 			m_salmon.should_move(1, false);
 			break;
-		case GLFW_KEY_RIGHT:
+		case GLFW_KEY_D:
 			m_salmon.should_move(2, false);
 			break;
-		case GLFW_KEY_DOWN:
+		case GLFW_KEY_S:
 			m_salmon.should_move(3, false);
 			break;
-		case GLFW_KEY_UP:
+		case GLFW_KEY_W:
 			m_salmon.should_move(4, false);
-			break;
-		case GLFW_KEY_A:
-			std::cout << "Advanced mode activated" << std::endl;
-			advanced_mode = true;
-			break;
-		case GLFW_KEY_B:
-			std::cout << "Basic mode activated" << std::endl;
-			advanced_mode = false;
-			break;
-		case GLFW_KEY_SPACE:
-			std::cout << "Missile fired" << std::endl;
-			if (advanced_mode)
-			{
-				spawn_fmissile();
-			}
 			break;
 		default:
 			break;
@@ -488,48 +285,12 @@ void World::on_key(GLFWwindow*, int key, int, int action, int mod)
 		glfwGetWindowSize(m_window, &w, &h);
 		m_salmon.destroy(); 
 		m_salmon.init();
-		m_turtles.clear();
-		m_fish.clear();
 		m_water.reset_salmon_dead_time();
-		m_current_speed = 1.f;
 	}
 
-	// Control the current speed with `<` `>`
-	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) &&  key == GLFW_KEY_COMMA)
-		m_current_speed -= 0.1f;
-	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) && key == GLFW_KEY_PERIOD)
-		m_current_speed += 0.1f;
-	
-	m_current_speed = fmax(0.f, m_current_speed);
 }
 
 void World::on_mouse_move(GLFWwindow* window, double xpos, double ypos)
 {
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-	// HANDLE SALMON ROTATION HERE
-	// xpos and ypos are relative to the top-left of the window, the salmon's 
-	// default facing direction is (1, 0)
-	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-	// 0 is forward (towards right of screen), pi is backwards (towards left)
-	// we want to set the range from 0 to 2pi (the unit circle), so we want to calculate the angle in radians
-	float rad = static_cast<float>(atan2(ypos - m_salmon.get_position().y, xpos - m_salmon.get_position().x));
-
-	// for some reason it's 1 pi off, so just subtract? haha
-	m_salmon.set_rotation(rad);
-
-
 }
 
-bool World::spawn_fmissile()
-{
-	Missile missile;
-	if (missile.init())
-	{
-		missile.set_position({ m_salmon.get_position().x,  m_salmon.get_position().y });
-		m_missiles.emplace_back(missile);
-		return true;
-	}
-	fprintf(stderr, "Failed to spawn missile");
-	return false;
-}
