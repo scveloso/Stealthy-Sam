a143d7c7e977ae33882d91c41ffe0655b604ccf1
d6g0b <d6g0b@ugrad.cs.ubc.ca>
2019-02-01 22:41:01 -0800

    new version

--- src/project_path.hpp
+++ src/project_path.hpp
@@ -2,4 +2,4 @@
 
 // Please don't change the content of this header
 
-#define PROJECT_SOURCE_DIR "C:/Users/Mah-dir Sheikh/Desktop/team05game/"
+#define PROJECT_SOURCE_DIR "C:/Users/carolinec/Desktop/cs436/test/"
