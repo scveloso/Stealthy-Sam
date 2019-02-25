4a11f484db36c8d526ab191c675bcde9876dee78
Sam <sam.goldwax@gmail.com>
2019-02-25 00:07:26 -0800

    added keys

--- src/CollisionSystem.hpp
+++ src/CollisionSystem.hpp
@@ -1,3 +1,4 @@
+#include <Components/GameStateCmp.hpp>
 #include "Components/CollisionCmp.hpp"
 #include "Components/TransformCmp.hpp"
 #include "Components/DrawCmp.hpp"
@@ -8,15 +9,17 @@
 class CollisionSystem
 {
 public:
-	CollisionSystem(ObjectManager om, CollisionCmp cc, TransformCmp tc);
+	CollisionSystem(ObjectManager om, CollisionCmp cc, TransformCmp tc, GameStateCmp* gsc);
 	int update(float elapsed_ms);
 	static bool AABB(Transform *tr1, Transform *tr2);
 	int handleDoors(Entity* entity);
 	int handleEnemies(Entity* entity);
+	int handleKeys(Entity* entity);
 	bool handleClosets(Entity* entity);
 
 private:
 	CollisionCmp collisionComponent;
 	TransformCmp transformComponent;
   ObjectManager objectManager;
+  GameStateCmp* gameStateComponent;
 };
