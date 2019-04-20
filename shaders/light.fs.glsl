11d7b8333bce0454f70430daf57f5df85b724fb9
Nicolas Gagnon <Nicolas.z.gagnon@gmail.com>
2019-04-19 22:37:04 -0700

    improvement on the light occlusion, this is Caroline's PR

--- shaders/light.fs.glsl
+++ shaders/light.fs.glsl
@@ -18,62 +18,134 @@ vec3 sp = vec3(s_position,1);
 float d_torch[5];
 
 float dimmer(float d) {
-
-	if (pow(d/1000, -0.1) > 1){
-		return 1.0;
+	if (pow(d/2000, -0.1) > 1){
+		return 1;
 	} else {
-		return pow(d/1000,-0.3);
+		return pow(d/2000,-0.3);
 	}
 }
+
+
 
 void main()
 {
     float d= (x - sp.x)*(x - sp.x)+(y- sp.y)*(y-sp.y);
     //create circle of light around each torch;
-    int j=0;
     for (int i = 0; i < 5; i++){
-      if (i==j){
-        d_torch[j]=(x- tor_position[j].x)*(x- tor_position[j].x)+ (y-tor_position[j].y)*(y-tor_position[j].y);
-      }
-      j++;
+        d_torch[i]=(x- tor_position[i].x)*(x- tor_position[i].x)+ (y-tor_position[i].y)*(y-tor_position[i].y);
     }
 
     int r = 150;
-    bool dt1 = d_torch[1] <= r*r;
-    bool dt2 = d_torch[2] <= r*r;
-    bool dt3 = d_torch[3] <= r*r;
-    bool dt4 = d_torch[4] <= r*r;
-    bool dt0 = d_torch[0] <= r*r;
-		// bool dt1 = d_torch[1] > 0;
-    // bool dt2 = d_torch[2] > 0;
-    // bool dt3 = d_torch[3] > 0;
-    // bool dt4 = d_torch[4] > 0;
-    // bool dt0 = d_torch[0] > 0;
-    float p = dimmer(d);
+		bool dt[5];
+    dt[1] = d_torch[1] <= r*r;
+    dt[2] = d_torch[2] <= r*r;
+    dt[3] = d_torch[3] <= r*r;
+    dt[4] = d_torch[4] <= r*r;
+    dt[0] = d_torch[0] <= r*r;
+		float k[5];
+		float p= dimmer(d);
 
-    if (death_cond ==0){
-      if (dt1 || dt2 || dt3 || dt4 || dt0)
-      {
-        float k;
-        if(dt1){k = dimmer(d_torch[1]);}
-        if(dt2){k = dimmer(d_torch[2]);}
-        if(dt3){k = dimmer(d_torch[3]);}
-        if(dt4){k = dimmer(d_torch[4]);}
-        if(dt0){k = dimmer(d_torch[0]);}
+		if (death_cond ==0){
+			if (dt[1] || dt[2] || dt[3] || dt[4] || dt[0] )
+			{
+				k[1] = dimmer(d_torch[1]);
+				k[2] = dimmer(d_torch[2]);
+				k[3] = dimmer(d_torch[3]);
+				k[4] = dimmer(d_torch[4]);
+				k[0] = dimmer(d_torch[0]);
 
-        if(p > k){
-          color= in_color*p;
-          }else{
-            color= in_color*k;
-          }
-        }
-      else if (time < 5*10){
-        color = in_color*p*(time/50);
-      }
-			else{
-				color= (in_color*p);
+				if(dt[0]){
+					if( p > k[0]){
+						color=in_color*p;
+					}
+					else if (dt[1] || dt[2] || dt[3] || dt[4]){
+						for(int i=0; i<5; i++){
+							if(k[i] <  k[0]){
+								color= 0.54*in_color*(9*k[i]+0.8*k[0]);
+							}
+							else{
+								color= in_color*k[0];
+							}
+						}
+					}
+						else{
+							color= in_color*k[0];
+						}
+					}
+
+				if(dt[1]){
+				  if( p > k[1]){
+				    color=in_color*p;
+				    }
+						else if (dt[0] || dt[2] || dt[3] || dt[4]){
+							for(int i=0; i<5; i++){
+								if(k[i] <  k[1] ){
+									color= 0.54*in_color*(9*k[i]+0.8*k[1]);
+								}else{
+									color= in_color*k[1];
+								}
+							}
+						}
+						else{
+				      color= in_color*k[1];
+				    }
+				  }
+				  if(dt[2]){
+				    if( p > k[2]){
+				      color=in_color*p;
+				      }
+							else if (dt[0] || dt[1] || dt[3] || dt[4]){
+								for(int i=0; i<5; i++){
+									if(k[i] < k[2] ){
+										color= 0.54*in_color*(9*k[i]+0.8*k[2]);
+									}else{
+										color= in_color*k[2];
+									}
+								}
+							}
+							else{
+				        color= in_color*k[2];
+				      }
+				    }
+				    if(dt[3]){
+				      if( p > k[3]){
+				        color=in_color*p;
+				        }
+								else if (dt[0] || dt[2] || dt[1] || dt[4]){
+									for(int i=0; i<5; i++){
+										if(k[i] <  k[3] ){
+											color= 0.54*in_color*(9*k[i]+0.8*k[3]);
+										}else{
+											color= in_color*k[3];
+										}
+									}
+								}
+								else{
+				          color= in_color*k[3];
+				        }
+				      }
+				      if(dt[4]){
+				        if( p > k[4]){
+				          color=in_color*p;
+				          }
+									else if (dt[0] || dt[2] || dt[3] || dt[1]){
+										for(int i=0; i<5; i++){
+											if(k[i] <  k[4] ){
+											 color= 0.54*in_color*(9*k[i]+0.8*k[4]);
+											}else{
+												color= in_color*k[4];
+											}
+										}
+									}
+									else{
+				            color= in_color*k[4];
+				          }
+				        }
 			}
-    }
+				else{
+					color= in_color*p;
+				}
+		}
 
     else if (death_cond == 1) {
       vec4 i= vec4(1,0,0,0.3);
